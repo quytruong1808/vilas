b131e0a5950ab525c2799fece9f2f65fede08f22
quytruong1991 <quytruong1991@gmail.com>
2015-08-27 13:24:32 +0700

    add remove button

--- LabpiLoad.py
+++ LabpiLoad.py
@@ -14,14 +14,22 @@ from kivy.uix.listview import ListView
 from kivy.adapters.listadapter import ListAdapter
 
 
+# global variable
+ItemId = 0
+
 class LoadScreen(Screen):
     boxlist_1 = ObjectProperty(None)
 
     def __init__(self, *args, **kwargs):
         super(LoadScreen, self).__init__(*args, **kwargs)
 
-        list_adapter = LoadAdapter(data=[], template='CustomListItem')
-        pdb_list = ListView(adapter=list_adapter, id='pdb_list_1')
+        list_item_args_converter = lambda row_index, obj: {'item_id': obj.item_id, 'text': obj.text, 'remove_item': obj.remove_item}
+
+        list_adapter = LoadAdapter(data=[], 
+                            args_converter=list_item_args_converter,
+                            template='CustomListItem')
+        pdb_list = ListView(adapter=list_adapter, 
+                            id='pdb_list_1')
         self.boxlist_1.add_widget(pdb_list)
 
     def show_load(self):
@@ -35,25 +43,47 @@ class LoadScreen(Screen):
         self._popup.dismiss()
 
     def load(self, path, filename):
-        for widget in self.boxlist_1.walk():
-            if(widget.id == 'pdb_list_1'):
-                widget.adapter.data.extend([os.path.join(path, filename[0])])
-                widget._trigger_reset_populate()
-
+        widget = self.findWidget(self.boxlist_1, 'pdb_list_1')
+        if(not widget is None):
+            global ItemId
+            ItemId += 1
+            widget.adapter.data.extend([DataItem(item_id=ItemId, text=os.path.join(path, filename[0]), remove_item=self.remove_item)])
+            widget._trigger_reset_populate()
 
         self.dismiss_popup()
+    
+    # Listview
+    def remove_item(self, item_id):
+        widget = self.findWidget(self.boxlist_1, 'pdb_list_1')
+        if(not widget is None):
+            for dataItem in widget.adapter.data:
+                if dataItem.item_id == item_id:
+                    print 'File '+str(dataItem.text)+' was removed!'
+                    widget.adapter.data.remove(dataItem)
+                    widget._trigger_reset_populate()
+                    break
+
+    def findWidget(self, layout, widget_id):
+        for widget in layout.walk():
+            if(widget.id == widget_id):
+                return widget
+        return None
+
     pass
 
 #--------------------------------------------------------#
-# Adapter class
+# ListView class
 class LoadAdapter(ListAdapter):
-    def remove_item(self):
-        if self.selection:
-            selection = self.selection[0].text
-            self.data.remove(selection)
-            # self.task_list._trigger_reset_populate()
-
+    
     pass
+
+
+
+class DataItem(object):
+    def __init__(self, item_id=0, text='', remove_item = ObjectProperty(None)):
+        self.item_id = item_id
+        self.text = text
+        self.remove_item = remove_item
 
 #--------------------------------------------------------#
 # Dialog class
