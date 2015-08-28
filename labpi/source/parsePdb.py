1a6379e8c911606d2d1e746137d5f6662b1341a8
quytruong1991 <quytruong1991@gmail.com>
2015-08-28 16:15:27 +0700

    add pip install

---
+++ labpi/source/parsePdb.py
@@ -0,0 +1,46 @@
+from prody import *
+
+class ParsePdb(object):
+    #Read pdb file
+    def __init__(self, Receptors = [], Ligands = [], char_system = ''):
+        self.Receptors = Receptors
+        self.Ligands = Ligands
+        self.char_system = char_system
+
+    def listChain(self, pdbFile):
+        listProtein = []
+        listLigand = []
+        listItems = []
+        input = parsePDB(pdbFile)
+
+        #Check protein
+        hierView = input.getHierView()
+        chains = list(hierView)
+        for y in range(0, len(chains)):
+            chain = chains[y].select('protein')
+            if chain is None: continue 
+            #get hierview again
+            chain = chain.getHierView()
+            listProtein.append(list(chain)[0])
+
+        #Check ligand
+        ligandView = input.select('not water and not ion and hetero')
+        if not ligandView is None:
+            #Get ligand array name
+            ligands = set(ligandView.getResnames())
+            for y in range(0, len(ligands)):
+                listLigand.append(ligandView.select('resname '+ligands.pop()))
+
+        for x in range(0, len(listProtein) + len(listLigand)):
+            if x < len(listProtein):
+                listItems.append("{0:8s} - {1:10s}{2:4s}{3:9s}".format(str(listProtein[x]), "protein - ", str(listProtein[x].numResidues()), ' residues'))
+            else: 
+                y = x - len(listProtein)
+                listItems.append("{0:8s} - {1:10s}{2:4s}{3:9s}".format(str(listLigand[y].getResnames()[0]), "ligand  - ", str(listLigand[y].numAtoms()), ' atoms'))
+
+        return listItems
+
+
+    pass
+
+  
